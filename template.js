9be09a45f1b5f7cc2389fc125e99228c66781484
na2kera <nat3mitaro@gmail.com>
2025-03-17 15:08:17 +0900

    テンプレート

---
+++ template.js
@@ -0,0 +1,12 @@
+function main(input) {
+  const a = input.split("\n");
+
+  console.log(a);
+}
+
+const test_input = `3
+  1 2 5`;
+
+main(test_input);
+
+// main(require("fs").readFileSync("/dev/stdin", "utf8"));
