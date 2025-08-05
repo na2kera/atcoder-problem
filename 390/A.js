8e83150a10fe4c06aa4ba610f9611ff028c6d54a
na2kera <nat3mitaro@gmail.com>
2025-08-05 14:44:22 +0900

    390-A

--- 390/A.js
+++ 390/A.js
@@ -1,23 +1,12 @@
 function main(input) {
   const a = input.split("\n");
-  const numArray = a[0].split(" ").map((str) => parseInt(str, 10));
 
-  let count = 0;
-  for (let i = 1; i < numArray.length; i++) {
-    if (numArray[i] < numArray[i - 1]) {
-      [numArray[i - 1], numArray[i]] = [numArray[i], numArray[i - 1]];
-      count++;
-    }
-  }
-
-  if (count == 1) {
-    console.log("Yes");
-  } else {
-    console.log("No");
-  }
+  console.log(a);
 }
 
-const test_input = `3 5 1 2 4`;
+const test_input = `5
+3 6 12 24 48
+`;
 
 main(test_input);
 
